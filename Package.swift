c80f01dd481f60693c41b2f142c394d4e3b58f49
Anand <anand.simplextech@gmail.com>
2026-07-18 00:48:21 +0530

    refactor(Package): committing missed out files

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "NSFeatureFreshMock",
+    platforms: [.iOS(.v17)],
+    products: [
+        .library(
+            name: "NSFeatureFreshMock",
+            targets: ["NSFeatureFreshMock"]),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/simplex-tech-apps/ns-core-design-system.git",
+            from: "1.2.0"
+        )
+    ],
+    targets: [
+        .target(
+            name: "NSFeatureFreshMock",
+            dependencies: [
+                .product(name: "NammaAppUI", package: "ns-core-design-system")
+            ]
+        )
+    ]
+)
