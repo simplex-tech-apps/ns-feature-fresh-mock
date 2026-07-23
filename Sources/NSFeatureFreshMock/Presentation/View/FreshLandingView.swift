e88417322a0cceb204b0d7a929cc12fd423ad0b4
Anand <anand.simplextech@gmail.com>
2026-07-23 21:34:30 +0530

    refactor(LandingPage): added category list to the landing page

--- Sources/NSFeatureFreshMock/Presentation/View/FreshLandingView.swift
+++ Sources/NSFeatureFreshMock/Presentation/View/FreshLandingView.swift
@@ -14,6 +14,110 @@ struct FreshLandingView: View {
     var freshViewModel: FreshViewModel
     
     var body: some View {
-        Text("Feature Fresh")
+        VStack {
+            ReusableHeaderView(style: .standard(title: "Shop by category"))
+            NAVerticalGrid_NxN_V1()
+            NASpotlightInRowViewV2()
+            ReusableHeaderView(style: .standard(title: "Top deals on bestsellers"))
+            NAHorizontalGrid_NxN_V1()
+            SeeAllButtonViewV2() {
+                
+            }
+            ReusableHeaderView(style: .standard(title: "Fresh Seasonal Delights"))
+            NAHorizontalGrid_NxN_V1()
+            SeeAllButtonViewV2() {
+                
+            }
+            ReusableHeaderView(style: .standard(title: "Breads and Buns"))
+            NAHorizontalGrid_NxN_V1()
+            SeeAllButtonViewV2() {
+                
+            }
+            ReusableHeaderView(style: .standard(title: "Daily Eggs upto 40% off"))
+            NAHorizontalGrid_NxN_V1()
+            SeeAllButtonViewV2() {
+                
+            }
+            ReusableHeaderView(style: .standard(title: "Cheese and Butter"))
+            NAHorizontalGrid_NxN_V1()
+            SeeAllButtonViewV2() {
+                
+            }
+            ReusableHeaderView(style: .standard(title: "Paneer & More!"))
+            NAHorizontalGrid_NxN_V1()
+            SeeAllButtonViewV2() {
+                
+            }
+            ReusableHeaderView(style: .standard(title: "Mango Mania"))
+            NAHorizontalGrid_NxN_V1()
+            SeeAllButtonViewV2() {
+                
+            }
+            ReusableHeaderView(style: .standard(title: "Summer Veggies"))
+            NAHorizontalGrid_NxN_V1()
+            SeeAllButtonViewV2() {
+                
+            }
+            ReusableHeaderView(style: .standard(title: "Yoghurts and Milkshakes"))
+            NAHorizontalGrid_NxN_V1()
+            SeeAllButtonViewV2() {
+                
+            }
+            ReusableHeaderView(style: .standard(title: "Batters & More!"))
+            NAHorizontalGrid_NxN_V1()
+            SeeAllButtonViewV2() {
+                
+            }
+            ReusableHeaderView(style: .standard(title: "Freshly made in small batches"))
+            NAHorizontalGrid_NxN_V1()
+            SeeAllButtonViewV2() {
+                
+            }
+            ReusableHeaderView(style: .standard(title: "Fresh Lassi, Smoothies & More!"))
+            NAHorizontalGrid_NxN_V1()
+            SeeAllButtonViewV2() {
+                
+            }
+            ReusableHeaderView(style: .standard(title: "Fresh chicken & seafood at best prices"))
+            NAHorizontalGrid_NxN_V1()
+            SeeAllButtonViewV2() {
+                
+            }
+            ReusableHeaderView(style: .standard(title: "Season's Special"))
+            NAHorizontalGrid_NxN_V1()
+            SeeAllButtonViewV2() {
+                
+            }
+            ReusableHeaderView(style: .standard(title: "Fresh organics"))
+            NAHorizontalGrid_NxN_V1()
+            SeeAllButtonViewV2() {
+                
+            }
+            ReusableHeaderView(style: .standard(title: "Curds"))
+            NAHorizontalGrid_NxN_V1()
+            SeeAllButtonViewV2() {
+                
+            }
+            ReusableHeaderView(style: .standard(title: "Almond Milk and Others"))
+            NAHorizontalGrid_NxN_V1()
+            SeeAllButtonViewV2() {
+                
+            }
+            ReusableHeaderView(style: .standard(title: "Fresh Salad Needs"))
+            NAHorizontalGrid_NxN_V1()
+            SeeAllButtonViewV2() {
+                
+            }
+            ReusableHeaderView(style: .standard(title: "Indian Breads"))
+            NAHorizontalGrid_NxN_V1()
+            SeeAllButtonViewV2() {
+                
+            }
+            ReusableHeaderView(style: .standard(title: "Plants for Your Garden"))
+            NAHorizontalGrid_NxN_V1()
+            SeeAllButtonViewV2() {
+                
+            }
+        }
     }
 }
